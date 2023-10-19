9b7544f2849bc74b56ee7727ecf4592d53554238
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2023-10-19 13:25:38 +0300

    lab4_3

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,40 +1,120 @@
 import random
 
+# Символи ASCII-арту
+characters = {
+    'a': [
+        "  AA  ",
+        " A  A ",
+        " AAAAA",
+        " A  A ",
+        " A  A "
+    ],
+    'b': [
+        " BBB  ",
+        " B  B ",
+        " BBBB ",
+        " B  B ",
+        " BBB  "
+    ],
+    'c': [
+        " CCCC ",
+        " C    ",
+        " C    ",
+        " C    ",
+        " CCCC "
+    ],
+    'e': [
+        " EEEE ",
+        " E    ",
+        " EEEE ",
+        " E    ",
+        " EEEE "
+    ],
+    'g': [
+        " GGGG ",
+        " G    ",
+        " G GG ",
+        " G  G ",
+        " GGGG "
+    ],
+    'i': [
+        " III ",
+        "  I  ",
+        "  I  ",
+        "  I  ",
+        " III "
+    ],
+    'r': [
+        " RRR  ",
+        " R  R ",
+        " RRR  ",
+        " R R  ",
+        " R  R "
+    ],
+    's': [
+        " SSSS ",
+        " S    ",
+        " SSSS ",
+        "    S ",
+        " SSSS "
+    ],
+    'y': [
+        " Y  Y ",
+        "  YY  ",
+        "   Y  ",
+        "   Y  ",
+        "   Y  "
+    ]
+}
+
+# Завдання 8: Варіанти кольорів
+color_option = input("Виберіть опцію кольорів (чорно-білий/відтінки сірого): ").lower()
+
 # Завдання 1: Введення користувача
-input_text = input("Введіть слово або фразу для ASCII-арту: " )
-
-# Завдання 2: Набір символів
-characters = "@#*%+=-:. "
-
-# Завдання 3: Розміри Art-у
-width = int(input("Введіть ширину ASCII-арту: "))
-height = int(input("Введіть висоту ASCII-арту: "))
+input_text = input("Введіть слово або фразу для ASCII-арту: ")
 
 # Завдання 5: Вирівнювання тексту
 alignment = input("Виберіть вирівнювання (ліво, центр, право): ").lower()
 if alignment not in ['ліво', 'центр', 'право']:
     alignment = 'ліво'
 
+# Завдання 3: Розміри Art-у
+width = int(input("Введіть ширину ASCII-арту: "))
+height = int(input("Введіть висоту ASCII-арту: "))
+
 # Завдання 4: Функція генерації Art-у
-def generate_ascii_art(input_text, characters, width, height, alignment):
+def generate_ascii_art(input_text, characters, alignment, width, height, color_option):
     lines = []
-    for _ in range(height):
-        line = ""
-        for _ in range(width):
-            line += random.choice(characters)
-        lines.append(line)
 
     if alignment == 'центр':
-        centered_lines = [line.center(width) for line in lines]
-        return centered_lines
+        lines.append(input_text.center(width))
     elif alignment == 'право':
-        right_aligned_lines = [line.rjust(width) for line in lines]
-        return right_aligned_lines
+        lines.append(input_text.rjust(width))
     else:
-        return lines
+        lines.append(input_text.ljust(width))
+
+    for i in range(5):
+        line = ""
+        for char in input_text:
+            if char.lower() in characters:
+                line += characters[char.lower()][i]
+            else:
+                line += " " * 6  # Пробіли, якщо символ не знайдено
+            line += " "  # Додатковий пробіл між буквами
+
+        if color_option == 'чорно-білий':
+            line = "\033[30m" + line + "\033[0m"  # Чорний колір тексту
+        elif color_option == 'відтінки сірого':
+            line = "\033[90m" + line + "\033[0m"  # Сірий колір тексту
+
+        lines.append(line)
+
+    return lines
+
+# Генерація ASCII-арту
+art = generate_ascii_art(input_text, characters, alignment, width, height, color_option)
 
 # Завдання 6: Відображення мистецтва
-art = generate_ascii_art(input_text, characters, width, height, alignment)
 for line in art:
     print(line)
 
@@ -52,15 +132,7 @@ def preview_ascii_art(art):
     for line in art:
         print(line)
 
-# Виклик функції попереднього перегляду
+# Попередній перегляд ASCII-арту
 preview_option = input("Бажаєте побачити попередній перегляд ASCII-арту? (так/ні): ").lower()
 if preview_option == 'так':
     preview_ascii_art(art)
-
-# Завдання 8: Варіанти кольорів (чорно-білий, відтінки сірого)
-color_option = input("Виберіть опцію кольорів (чорно-білий/відтінки сірого): ").lower()
-if color_option == 'відтінки сірого':
-    grayscale_characters = "@%#*+=-:. "
-    grayscale_art = generate_ascii_art(input_text, grayscale_characters, width, height, alignment)
-    for line in grayscale_art:
-        print(line)
